d8311d338e48a7cc4a2250afd2a0bef1d164768f
Ugo3007 <ugo.gabriel@outlook.com>
2023-06-26 10:32:52 +0200

    Included AboutContainer in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
 import HeaderNavBar from "@/components/HeaderNavBar.vue";
+import AboutContainer from "@/components/AboutContainer.vue";
 </script>
 
 <template>
   <HeaderNavBar/>
+  <AboutContainer/>
 </template>
 
 <style scoped>
