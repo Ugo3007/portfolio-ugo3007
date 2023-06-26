ab7636ae4c30dc174343f77a7629b2d3f96b2d9a
Ugo3007 <ugo.gabriel@outlook.com>
2023-06-26 10:01:59 +0200

    🚧 First version of HeaderNavBar.vue, needs to be made pretty

--- src/components/HeaderNavBar.vue
+++ src/components/HeaderNavBar.vue
@@ -3,9 +3,41 @@
 </script>
 
 <template>
-  <main></main>
+  <header>
+    <section>Logo</section>
+    <section>
+      <ul>
+        <li>Home</li>
+        <li>À propos</li>
+        <li>Projets</li>
+        <li>CV</li>
+      </ul>
+    </section>
+  </header>
 </template>
 
 <style scoped>
+  header {
+    width: 100%;
+    height: 64px;
+    display: flex;
+    justify-content: space-between;
+    background-color: #FFEFDF;
+  }
+
+  section {
+    display: flex;
+    align-items: center;
+    margin: 0 10px;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+  }
+
+  li {
+    margin: 0 10px;
+  }
 
 </style>
