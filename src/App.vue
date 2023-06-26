c2246a90803d44f80f4a8d63585c679a29deb066
Ugo3007 <ugo.gabriel@outlook.com>
2023-06-26 09:51:15 +0200

    ✨ Created first component "HeaderNavBar"

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 
+import HeaderNavBar from "@/components/HeaderNavBar.vue";
 </script>
 
 <template>
-  <main>Hello World</main>
+  <HeaderNavBar/>
 </template>
 
 <style scoped>
