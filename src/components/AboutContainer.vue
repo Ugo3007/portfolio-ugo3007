17636d8722c0cc4249f55e3c7b2ef153e657ca19
Ugo3007 <ugo.gabriel@outlook.com>
2023-06-26 10:31:34 +0200

    ✨💄 New component AboutContainer.vue that'll contain bitmoji logo and some placeholder text

---
+++ src/components/AboutContainer.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <div class="logo">
+      <h2>Logo</h2>
+    </div>
+    <div>
+      <p>Lorem Elsass ipsum Pellentesque tellus hopla bredele ch'ai merci vielmols amet dignissim suspendisse habitant non Miss Dahlias varius Chulia Roberstau quam, Huguette barapli Gal ! blottkopf, rhoncus gewurztraminer wie hopla Spätzle sed DNA, knack mamsell vielmols, id commodo libero. schneck pellentesque  morbi amet, ullamcorper quam. Chulien leo nullam ac non id, sit kougelhopf salu ftomi! hopla consectetur und libero, tchao bissame baeckeoffe mänele météor elementum so kuglopf gal Oberschaeffolsheim purus et Racing. amet adipiscing réchime ante Mauris in, semper nüdle hopla vulputate turpis geïz schnaps ornare lacus sit Wurschtsalad gravida auctor, Coopé de Truchtersheim chambon condimentum Pfourtz!</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    box-sizing: border-box;
+    padding: 0 86px 0 65px;
+    background-color: #6EBDE5;
+    max-width: 1440px;
+    width: 100%;
+    height: 448px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .logo {
+    width: 250px;
+    height: 250px;
+    background-color: #FFEFDF;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  p {
+    width: 900px;
+    color: rgba(0, 0, 0, 0.87);
+    font-size: 20px;
+    font-family: Roboto, sans-serif;
+    line-height: 28px;
+  }
+</style>
