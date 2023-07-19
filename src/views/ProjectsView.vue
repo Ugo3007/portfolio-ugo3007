8c69c45daf8efe9f071ddaec36785891a8190691
Ugo3007 <ugo.gabriel@outlook.com>
2023-07-19 14:21:03 +0200

    ✨ New view ProjectsView.vue, to display all the projects

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <section>
+      <h2>Mes projets</h2>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+
+main {
+  max-width: 1440px;
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+}
+
+section {
+  max-width: 1020px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+h2 {
+  font-family: Montserrat, sans-serif;
+}
+</style>
