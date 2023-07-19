ff54748e95dfdbb6ec010e3d91c4a18ff79e4e0c
Ugo3007 <ugo.gabriel@outlook.com>
2023-07-19 14:23:27 +0200

    🚚 New route "home" to display ProjectsView.vue while on homepage

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'projects',
+      // @ts-ignore
+      component: () => import('../views/ProjectsView.vue')
+    }
     /*{
       path: '/',
       name: 'home',
